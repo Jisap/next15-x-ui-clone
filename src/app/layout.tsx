538e1c09c06957cfbbefb9fb0ff71d2fabb79f9e
Jorge Matos <jisapjisap@gmail.com>
2025-01-31 16:55:49 +0100

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
-          <div className="px-2 xsm:px-4 xxl:px-8 bg-red-200 h-screen">
+          <div className="px-2 xsm:px-4 xxl:px-8">
             <LeftBar />
           </div>
-          <div className="px-2 bg-red-200 h-screen">{children}</div>
-          <div className="px-2 bg-red-200 h-screen">
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-gray-100">{children}</div>
+          <div className="flex-1 hidden lg:flex ml-4 md:ml-8">
             <RightBar />
           </div>
         </div>
