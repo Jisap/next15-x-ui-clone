f57bd8919c8d153ee5a0573db37991c4fe637ebd
Jorge Matos <jisapjisap@gmail.com>
2025-01-31 18:12:49 +0100

    add Image component with ImageKit integration and update configuration

---
+++ src/components/Image.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { IKImage } from "imagekitio-next";
+
+type ImageType = {
+  path: string;
+  w?: number;
+  h?: number;
+  alt: string;
+  className?: string;
+  tr?: boolean;         // booleano opcional que indica si se deben aplicar transformaciones a la imagen.
+};
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
+if (!urlEndpoint) {
+  throw new Error('Error: Please add urlEndpoint to .env or .env.local')
+}
+
+const Image = ({ path, w, h, alt, className, tr }: ImageType) => {
+  return (
+    <IKImage
+      urlEndpoint={urlEndpoint}
+      path={path}
+      {...(tr
+        ? { transformation: [{ width: `${w}`, height: `${h}` }] }      // Si tr es true, el ancho y el alto se aplican como transformaciones en ImageKit -> redimension en el server antes de servirla al cliente 
+        : { width: w, height: h })}                                    // Si tr es false o no está definido, el ancho y el alto se aplican directamente como atributos HTML -> no redimension en el server
+      lqip={{ active: true, quality: 20 }}                             // Activa la carga de imágenes de baja calidad (LQIP) con una calidad del 20%.
+      alt={alt}
+      className={className}
+    />
+  );
+};
+
+export default Image;
