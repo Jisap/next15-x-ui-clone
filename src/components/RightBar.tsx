16b626f901baeb310056893c5ffa7ddd3b30bd70
Jorge Matos <jisapjisap@gmail.com>
2025-02-02 13:44:46 +0100

    feat: add PopularTags and Recomendations components; integrate Search in RightBar

--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Link from "next/link";
+import Search from './Search'
+import PopularTags from './PopularTags'
+import Recomendations from './Recomendations'
 
 const RightBar = () => {
   return (
-    <div>RightBar</div>
+    <div>
+      <Search />
+      <PopularTags />
+      <Recomendations />
+      <div className="text-textGray text-sm flex gap-x-4 flex-wrap">
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+        <Link href="/">Cookie Policy</Link>
+        <Link href="/">Accessibility</Link>
+        <Link href="/">Ads Info</Link>
+        <span>© 2025 J Corp.</span>
+      </div>
+    </div>
   )
 }
 
