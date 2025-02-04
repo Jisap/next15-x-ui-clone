f73273bb87e8a8ecbfb0ed7dd719579559d0ae39
Jorge Matos <jisapjisap@gmail.com>
2025-02-04 15:04:31 +0100

    feat: create BoardLayout component for structured layout with LeftBar and RightBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,23 @@
-import LeftBar from "@/components/LeftBar";
 import "./globals.css";
-import RightBar from "@/components/RightBar";
+import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 
-export default function RootLayout({
+
+export const metadata: Metadata = {
+  title: "Lama Dev X Clone",
+  description: "Next.js social media application project",
+};
+
+export default function AppLayout({
   children,
-  modal
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider> 
       <html lang="en">
-        <body>
-          <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
-            <div className="px-2 xsm:px-4 xxl:px-8">
-              <LeftBar />
-            </div>
-            <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
-              {children}
-              {modal}
-            </div>
-            <div className="flex-1 hidden lg:flex ml-4 md:ml-8">
-              <RightBar />
-            </div>
-          </div>
-        </body>
-      </html>
+        <body>{children}</body>
+      </html> 
     </ClerkProvider>
   );
 }
