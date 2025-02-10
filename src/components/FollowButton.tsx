cbc2b50d2c2bf1e33093c8eac25a5e88f07f0679
Jorge Matos <jisapjisap@gmail.com>
2025-02-10 16:09:18 +0100

    feat: implement optimistic UI for follow/unfollow button

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { followUser } from "@/action";
+import { useOptimistic, useState } from "react";
+
 interface Props {
   userId: string
   isFollowed: boolean
@@ -7,12 +10,27 @@ interface Props {
 
 const FollowButton = (props: Props) => {
   
-  const { userId, isFollowed } = props
+  const { userId, isFollowed } = props;
+
+  const [state, setState] = useState(isFollowed); // Estado real de si esta seguido o no
+
+  const [optimisticFollow, switchOptimisticFollow] = useOptimistic(
+    state,           // Estado base (optimisticFollow)
+    (prev) => !prev  // Función que cambia el estado de forma inmediata (switchOptimisticFollow)
+  )
+
+  const followAction = async () => {
+    switchOptimisticFollow("")   // Se cambia inmediatamente el estado de optimisticFollow a true
+    await followUser(userId);    // Se hace la petición al server para cambiar el estado
+    setState((prev) => !prev)    // Si la res es ok se cambia el estado real state=true. Cuando state cambia, optimisticFollow también se actualiza automáticamente.
+  };
 
   return (
-    <button className="py-2 px-4 bg-white text-black font-bold rounded-full">
-      {isFollowed ? "Unfollow" : "Follow"}
-    </button>
+    <form action={followAction}>
+      <button className="py-2 px-4 bg-white text-black font-bold rounded-full">
+        {optimisticFollow ? "Unfollow" : "Follow"}
+      </button>
+    </form>
   )
 }
 
