d596d873413791973a3bd091dfb0eba0b61f9320
Jorge Matos <jisapjisap@gmail.com>
2025-02-11 16:09:46 +0100

    feat: enhance Notification component with image and improved layout

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,10 +1,29 @@
 "use client"
 
+import Image from "./Image";
 
+
+
+type NotificationType = {
+  id: string;
+  senderUsername: string;
+  type: "like" | "comment" | "rePost" | "follow";
+  link: string;
+};
 
 const Notification = () => {
   return (
-    <div>Notification</div>
+    <div className='cursor-pointer p-2 rounded-full hover:bg-[#181818] flex items-center gap-4'>
+      <Image
+        path={`icons/notification.svg`}
+        alt="notification"
+        w={24}
+        h={24}
+      />
+      <span className='hidden xxl:inline'>
+        Notifications
+      </span>
+    </div>
   )
 }
 
